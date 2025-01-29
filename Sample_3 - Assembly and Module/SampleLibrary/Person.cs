b5fc40ebadc35ee6c4b2da717f6534d97900606c
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2025-01-29 16:00:31 +0700

    Add files via upload

---
+++ Sample_3 - Assembly and Module/SampleLibrary/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleLibrary
+{
+    public enum EPersonMaritalStatus
+    {
+        Married,
+        Single
+    }
+
+    [Serializable]
+    public class Person
+    {
+        public string Name;
+        public string LastName;
+        public int Age;
+        public EPersonMaritalStatus MaritalStatus;
+
+        public Person(string name, string lastName, int age)
+        {
+            Name = name;
+            LastName = lastName;
+            Age = age;
+            MaritalStatus = EPersonMaritalStatus.Single;
+        }
+
+        public string GetInfoToString()
+        {
+            return $"Имя: {Name}\n" +
+                $"Фамилия: {LastName}\n" +
+                $"Возраст: {Age}";
+        }
+    }
+
+    public class Employee : Person
+    {
+        public string Position;
+        public decimal Salary;
+
+        public Employee(string name, string lastName, int age,
+            string position, decimal salary) : 
+            base(name, lastName, age) 
+        {
+            Position = position;
+            Salary = salary;
+        }
+    }
+}
---
+++ Sample_3 - Assembly and Module/Sample_3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+
+// Сборка (assembly) – запущенное приложение, JIT-скомпилированное и выгруженное в
+// оперативную память. Как правило, сборка состоит из некоторого количества
+// совместно исполняемых модулей, делящих одно и то же адресное пространство.
+
+// Модуль (module) – под модулем мы будем понимать
+// некоторый исполняемый файл: *.exe или *.dll, который
+// загружен, как составной элемент некоторой сборки.
+
+// Пример взаимодействия с объектами Assembly и Module
+namespace Sample_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Загрузить библиотеку 
+            Assembly asm = Assembly.Load(AssemblyName.GetAssemblyName("SampleLibrary.dll"));
+
+            // Получить модули этой сборки 
+            Module module = asm.GetModule("SampleLibrary.dll");
+
+            // Вывод типов данных, объевленные в модуле 
+            foreach(Type t in module.GetTypes())
+                Console.WriteLine(t.FullName);
+
+            // Получить тип данных из сборки 
+            Type Person = module.GetType("SampleLibrary.Person");
+
+            // Создаем объект из полученного типа
+            object person = Activator.CreateInstance(Person, 
+                new object[] {"Имя1", "Фамилия1", 30 });
+
+            // Вызываем метод созданного объекта
+            string dataPerson = Person.GetMethod("GetInfoToString").Invoke(person, null).ToString();
+            Console.WriteLine($"Данные человека:\n{dataPerson}");
+
+            Console.ReadLine();
+        }
+    }
+}
